a4c92af9981d5fb9fafde9b835d60d9f4f3eb6fb
Alex Neigher <alex.neigher@snapdocs.com>
2017-10-20 17:33:40 -0600

    company edit

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -18,7 +18,12 @@ class CompaniesController < ApplicationController
 
   def edit
     @company = current_user.company
-    
+  end
+
+  def update
+    @company = current_user.company
+    @company.update(company_params)
+    redirect_to company_path(@company)
   end
 
   def index
