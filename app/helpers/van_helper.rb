349ea748e923058c8b6e0d9aa5eb579daa10029f
Alex Neigher <alex.neigher@snapdocs.com>
2017-10-19 18:12:09 -0600

    safe for no features

--- app/helpers/van_helper.rb
+++ app/helpers/van_helper.rb
@@ -1,7 +1,10 @@
 module VanHelper
 
   def van_feature_tags(tags_string)
-    tag_array = tags_string.split(',')
+    tag_array = tags_string.try(:split, ',')
+
+    return nil unless tag_array.any?
+
     tag_array.map do |tag|
       content_tag(:div, tag, class: 'feature-tag')
     end.join(' ').html_safe
