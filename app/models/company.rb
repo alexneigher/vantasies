2365323ce985c28131c51aac199467c08a3c0397
Alex Neigher <alex.neigher@snapdocs.com>
2017-10-24 20:51:34 -0600

    force absolute url

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,6 +6,8 @@ class Company < ApplicationRecord
 
   validates_uniqueness_of :user_id
 
+  before_save :website_url_is_absolute
+
   def card_photo
     return "https://images.unsplash.com/photo-1491314768749-82db5ebd6499?w=400" unless photos.any?
     photos.ordered.first.image.url(:medium)
@@ -20,4 +22,10 @@ class Company < ApplicationRecord
     return true if user_id == current_user.try(:id) #this person is the author
     return true if is_paid?
   end
+
+  private
+    def website_url_is_absolute
+      absolute_url = self.website_url.gsub('https://','').gsub('http://','').prepend('https://')
+      self.website_url = absolute_url
+    end
 end
