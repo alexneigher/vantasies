c7205425635d3b1bbb4bf841c9ef850fbc8d8e10
Alex Neigher <alex.neigher@snapdocs.com>
2017-10-24 21:29:23 -0600

    photo modal from carousel

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,20 @@
 class PhotosController < ApplicationController
 
-
+  #ued for testing only
   def new
     @photo = Photo.new
   end
 
+  #used to render a photo from a carousel
   def show
     @photo = Photo.find(params[:id])
+    respond_to do |format|
+      format.js
+      format.html
+    end
   end
 
+  #used for testing only
   def create
     @photo = Photo.new(photo_params.merge(company_id: 2))
 
